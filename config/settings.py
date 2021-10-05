b2bc49d25822f0d5a8caae722c342b3408265ddc
Patrick <pat_pi@yahoo.com>
2021-10-05 17:58:12 +0200

    create account tiers script in migrations

--- config/settings.py
+++ config/settings.py
@@ -153,3 +153,9 @@ AUTH_USER_MODEL = 'users.CustomUser'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 IMAGE_TYPES = ['jpeg', 'png']
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
